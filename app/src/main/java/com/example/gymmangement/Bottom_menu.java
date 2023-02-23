01032993bd1ba1340529c8a611ec8d39057c8954
ritesh1709 githup <riteshkanojiya5804@gmail.com>
2023-02-23 23:14:53 +0530

    perfext

--- app/src/main/java/com/example/gymmangement/Bottom_menu.java
+++ app/src/main/java/com/example/gymmangement/Bottom_menu.java
@@ -1,14 +1,37 @@
 package com.example.gymmangement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.gymmangement.databinding.ActivityMainBinding;
+
 public class Bottom_menu extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_menu);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnSelectedListener(item ->{
+
+            switch (item.getItemId())
+        })
     }
-}
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+    }
